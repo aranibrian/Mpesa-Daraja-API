b48d87680a7ae5a155fb7905532fabac8c896800
aranibrian <aranibrian@gmail.com>
2023-12-11 20:30:01 +0300

    Creating Repo and Payment form

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mpesa Daraja API</title>
+</head>
+<body>
+
+	<!-- Start Form to handle Payments -->
+	<form id="mpesaPaymentForm" method="post">
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" required>
+    
+    <label for="amount">Amount:</label>
+    <input type="number" id="amount" name="amount" required>
+    
+    <label for="phone">Phone Number:</label>
+    <input type="tel" id="phone" name="phone" required placeholder="2547XXXXXXXX">
+    
+    <input type="submit" value="Pay with M-Pesa">
+	</form>
+	<!-- End Form to handle Payments -->
+
+	<!-- Start Display the Payment Status -->
+	<div id="paymentStatus"></div>
+	<!-- Start Display the Payment Status -->
+
+</body>
+</html>
